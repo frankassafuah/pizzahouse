b37e1bb7b877c438e93e02fea75600e989810af6
frankassafuah <frankassafuah@gmail.com>
2023-10-26 18:39:38 +0000

    implement blade content

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,21 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/pizzas', function () {
+    // get data from a database
+    $pizzas = [
+      ['type' => 'hawaiian', 'base' => 'cheesy crust'],
+      ['type' => 'volcano', 'base' => 'garlic crust'],
+      ['type' => 'veg supreme', 'base' => 'thin & crispy']
+    ];
+  
+    return view('pizzas', [
+      'pizzas' => $pizzas,
+    ]);
+  });
+  
+  Route::get('/pizzas/{id}', function ($id) {
+    // use the $id variable to query the db for a record
+    return view('details', ['id' => $id]);
+  });
